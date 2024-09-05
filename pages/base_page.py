1a1cb1782fa20dd905d4310572c6ae4345d6573a
AdelZA-it05 <a.aydamirov70@gmail.com>
2024-09-05 12:45:47 +0300

    что то не добавилось

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
-        print("Learned Page Object")
+        self.browser.get(self.url)
